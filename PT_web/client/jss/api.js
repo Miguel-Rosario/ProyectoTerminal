6767a7a2f67f69d39156ffc20a6c8b7a0fb3832a
Miguel-Rosario <126648916+Miguel-Rosario@users.noreply.github.com>
2025-09-21 13:02:17 -0600

    Update api.js

--- PT_web/client/jss/api.js
+++ PT_web/client/jss/api.js
@@ -1,1 +1,25 @@
+const API_URL = "http://127.0.0.1:5000"; // tu backend Flask
+
+async function apiGet(endpoint) {
+  try {
+    const response = await fetch(API_URL + endpoint);
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error en apiGet:", error);
+    return [];
+  }
+}
+
+async function apiPost(ruta, datos) {
+  const resp = await fetch(`${API_URL}${ruta}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(datos)
+  });
+  return resp.json();
+}
+
 
